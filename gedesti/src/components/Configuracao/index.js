2630d6f767c198131168b06dac8193ceea444feb
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-10-06 19:51:43 -0300

    change configs :100:

--- gedesti/src/components/Configuracao/index.js
+++ gedesti/src/components/Configuracao/index.js
@@ -4,19 +4,52 @@ import Nav from "../Fixed/Nav"
 import Title from "../Fixed/Search/Title";
 
 
-export default function Configuracao() {
+export default function Configuration() {
     return (
-        <div className="configuracao">
+        <div className="configuration">
             <Header />
             <Nav />
 
             <div className="container">
-                <div className="fundo-titulo">
+                <div className="background-title">
                     <Title nav="Configurações" title="Configurações" />
                 </div>
 
-                <div className="caixa"></div>
-        
+                <div className="box">
+                    <div className="profile">
+                        <img className="picture-profile" src="https://media-exp1.licdn.com/dms/image/C5603AQGoPhhWyeL2-Q/profile-displayphoto-shrink_200_200/0/1516833080377?e=2147483647&v=beta&t=O_q0eYPuycqoRh8ACadEX5gQhrVbPnomvJKRFQTIycI" alt="" />
+                        <div className="email-name">
+                            <div className="flex">
+                                <span className="name">Jair Paulo Satig</span>
+                                <span class="material-symbols-outlined">
+                                    edit
+                                </span>
+                            </div>
+                            <span className="email">jair@weg.net</span>
+                        </div>
+                    </div>
+
+                    <div className="change-configuration">
+                        <div className="flex">
+                            <span className="title-confuration">Senha</span>
+                            <span class="material-symbols-outlined">
+                                edit
+                            </span>
+                        </div>
+                        <span className="subtitle-confuration">Alterar sua senha</span>
+                    </div>
+
+                    <div className="change-configuration">
+                        <span className="title-confuration">Acessibilidade</span>
+                        <span className="subtitle-confuration">Tamanho da fonte</span>
+                    </div>
+
+                    <div className="change-configuration">
+                        <span className="title-confuration">Notificações</span>
+                        <span className="subtitle-confuration">Notificações de mensagens</span>
+                    </div>
+                </div>
+
             </div>
 
         </div>
--- gedesti/src/components/Mensagens/Mensagem/index.js
+++ gedesti/src/components/Mensagens/Mensagem/index.js
@@ -7,7 +7,7 @@ export default function Mensagem() {
             <div className="perfil">
                 <img className="foto-usuario" src="https://media-exp1.licdn.com/dms/image/C5603AQGoPhhWyeL2-Q/profile-displayphoto-shrink_200_200/0/1516833080377?e=2147483647&v=beta&t=O_q0eYPuycqoRh8ACadEX5gQhrVbPnomvJKRFQTIycI" alt="" />
                 <div className="nome-mensagem">
-                    <span className="nome-usuario">Jair - Analista da demanda 01</span>
+                    <span className="nome-usuario">Jair Paulo Satig - Analista da demanda 01</span>
                     <span className="ultima-mensagem">Eduarda: Demanda ok!</span>
                 </div>
             </div>

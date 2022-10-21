9989362c2ff9485f46327050804db0d0d078d9e2
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-10-20 21:28:24 -0300

    🔄 progress bar

--- gedesti/src/components/CrateDemand/ProgressBar/index.js
+++ gedesti/src/components/CrateDemand/ProgressBar/index.js
@@ -2,17 +2,61 @@ import "./style.css"
 
 export default function ProgressBar(props) {
 
-    return (
-        <div className="progress-bar">
-            <div className="ellipse-current">
-                <span className="material-symbols-outlined">
-                    done
-                </span>
+    if (props.atual == "1") {
+        return (
+            <div className="progress-bar">
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+                <div className="line"></div>
+                <div className="ellipse">2</div>
+                <div className="line"></div>
+                <div className="ellipse">3</div>
             </div>
-            <div className="line"></div>
-            <div className="ellipse">2</div>
-            <div className="line"></div>
-            <div className="ellipse">3</div>
-        </div>
-    );
+        );
+    } else if (props.atual == "2") {
+        return (
+            <div className="progress-bar">
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+                <div className="line-current"></div>
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+                <div className="line"></div>
+                <div className="ellipse">3</div>
+            </div>
+        );
+    } else if (props.atual == "3") {
+        return (
+            <div className="progress-bar">
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+                <div className="line-current"></div>
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+                <div className="line-current"></div>
+                <div className="ellipse-current">
+                    <span className="material-symbols-outlined">
+                        done
+                    </span>
+                </div>
+            </div>
+        );
+    }
+
+
 }

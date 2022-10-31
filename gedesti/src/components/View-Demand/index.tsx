a845c4d66ee7b5114698f48ca562468aec842bb4
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-10-31 16:13:15 -0300

    ✔

--- gedesti/src/components/View-Demand/index.tsx
+++ gedesti/src/components/View-Demand/index.tsx
@@ -21,7 +21,30 @@ export default function ViewDemand() {
 
                 <div className="box">
 
-                    <p>Informações Gerais</p>
+                    <p>Situação a ser Resolvida (Situação Atual)</p>
+
+                    <span>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum eleifend quam vitae viverra. Nullam vulputate elit a ipsum porttitor gravida. Proin et vehicula velit. Donec eget nulla quis turpis placerat molestie a sed justo. Curabitur sit amet pellentesque metus. Donec dictum elit a libero aliquam aliquam. Nullam porttitor justo pharetra orci porttitor, id tristique diam pulvinar. Mauris facilisis, dolor et aliquam elementum, ex ligula ullamcorper tellus, a consequat purus augue at felis. Aliquam ante lectus, lobortis sit amet orci quis, porta lobortis turpis. Suspendisse aliquet vulputate sapien, non fringilla erat ultrices at. Proin elementum orci ut ante aliquet sollicitudin.
+                    </span>
+
+                    <p className="title">Proposta</p>
+
+                    <span>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum eleifend quam vitae viverra. Nullam vulputate elit a ipsum porttitor gravida. Proin et vehicula velit. Donec eget nulla quis turpis placerat molestie a sed justo. Curabitur sit amet pellentesque metus. Donec dictum elit a libero aliquam aliquam. Nullam porttitor justo pharetra orci porttitor, id tristique diam pulvinar. Mauris facilisis, dolor et aliquam elementum, ex ligula ullamcorper tellus, a consequat purus augue at felis. Aliquam ante lectus, lobortis sit amet orci quis, porta lobortis turpis. Suspendisse aliquet vulputate sapien, non fringilla erat ultrices at. Proin elementum orci ut ante aliquet sollicitudin.
+                    </span>
+
+                    <p className="title">Centro de Custos</p>
+
+                    <hr />
+
+                    <span>Codigo</span>
+
+                    <span>Nome</span>
+
+
+                    <hr />
+
+                    <span>55435235345</span>
 
                 </div>
 

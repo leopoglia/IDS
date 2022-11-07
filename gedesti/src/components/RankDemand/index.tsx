0571a313f4f457a72146449d61730b24f9db00dc
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-11-07 18:45:53 -0300

    ✔

---
+++ gedesti/src/components/RankDemand/index.tsx
@@ -0,0 +1,15 @@
+import "./style.css"
+import { useState } from "react";
+
+
+export default function RankDemand() {
+
+
+    return (
+        <div className="rank-demand">
+
+
+
+        </div >
+    );
+}

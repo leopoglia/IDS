7c42f2e5a8261476078408e77c59435c7967e625
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-11-16 16:54:05 -0300

    ✔

--- gedesti/src/components/Demands/index.tsx
+++ gedesti/src/components/Demands/index.tsx
@@ -10,46 +10,78 @@ export default function Demands() {
 
     const url = window.location.href.split("/");
 
-    console.log(url[3])
-
     const [table, setTableList] = useState(false);
+
+    const footer = () => {
+        return (
+            <div>
+                <div className="navigator" >
+                    <div className="current">1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                    <div>{">"}</div>
+                </div >
+                <Footer />
+            </div >
+        )
+    }
 
     const setTable = () => {
         setTableList(!table);
     }
 
     return (
-        <div className="demands">
-            <Header icon="folder_copy" title="Demandas" />
-            <Nav />
+        <div>
+            {(url[3] === "demands") ? (
+                <div className="demands">
+                    <Header icon="folder_copy" title="Demandas" />
+                    <Nav />
 
-            <div className="container">
-                <Search nav="Demandas > Visualizar Demandas" title="Demandas" button="Criar Demand" link="/demand/create/1" setTable={setTable} />
-
-
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-                <Demand listDirection={table} />
-
-
-                <div className="navigator">
-                    <div className="current">1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>{">"}</div>
-
+                    <div className="container">
+                        <Search nav="Demandas > Visualizar Demandas" title="Demandas" button="Criar Demand" link="/demand/create/1" setTable={setTable} />
+                        <Demand listDirection={table} />
+                        {footer()}
+                    </div>
                 </div>
 
-                <Footer />
-
-            </div>
-
+            ) : (url[3] === "proposals") ? (
+                <div className="proposals">
+                    <Header icon="content_paste" title="Propostas" />
+                    <Nav />
+                    <div className="container">
+                        <Search nav="Propostas > Visualizar Propostas" title="Propostas" button="Criar Proposta" link="/demands" setTable={setTable} />
+                        <Demand listDirection={table} />
+                        {footer()}
+                    </div>
+                </div>
+            ) : (url[3] === "agendas") ? (
+                <div className="agendas">
+                    <Header icon="file_copy" title="Pautas" />
+                    <Nav />
+                    <div className="container">
+                        <Search nav="Pautas > Visualizar Pautas" title="Pautas" button="Criar Pauta" link="/agenda/create" setTable={setTable} />
+                        <Demand listDirection={table} />
+                        {footer()}
+                    </div>
+                </div>
+            ) : (url[3] === "minutes") ? (
+                <div className="minutes">
+                    <Header icon="description" title="Atas" />
+                    <Nav />
+                    <div className="container">
+                        <Search nav="Atas > Visualizar Atas" title="Atas" button="Criar Ata" link="/minutes/create" setTable={setTable} />
+                        <Demand listDirection={table} />
+                        {footer()}
+                    </div>
+                </div>
+            ) : (
+                <div className="null" />
+            )
+            }
 
         </div>
-    );
+
+    )
+
 }

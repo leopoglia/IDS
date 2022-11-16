7140dbfb7af8f9465707ee3ae02f25de21081060
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-11-16 13:25:49 -0300

    ✔

--- gedesti/src/services/userService.ts
+++ gedesti/src/services/userService.ts
@@ -6,9 +6,11 @@ const url = "https://backend-ten-sigma.vercel.app/api";
 
 const Services = {
 
-    cadastrarCliente: function (nome:String, cnpj:String) {
+    cadastrarCliente: function (nome: String, cnpj: String) {
         return new Promise((resolve, reject) => {
             fetch(url + '/cliente', { method: 'POST', body: JSON.stringify({ nome: nome, cnpj: cnpj }), headers: { 'Content-Type': 'application/json' } }).then(function (result) { return result.json(); }).then(resolve).catch(resolve)
         })
     },
 }
+
+export default Services;

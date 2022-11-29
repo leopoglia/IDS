2ab20e54f5ff207e0d5384ba42d8f8a09fbf763a
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-11-28 21:47:00 -0300

    ✔

--- gedesti/src/components/Solicitation/Proposals/CommissionOpinion/index.tsx
+++ gedesti/src/components/Solicitation/Proposals/CommissionOpinion/index.tsx
@@ -1,13 +1,15 @@
 import Header from "../../../Fixed/Header";
 import Nav from "../../../Fixed/Nav";
 import Title from "../../../Fixed/Search/Title";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 
 export default function CommissionOpinion() {
     return (
         <div className="commission-opinion">
 
-            <Header title="Commission Opinion" icon="edit" />
+            <Header title="Parecer da  Comissão" icon="edit" />
 
             <Nav />
 
@@ -15,12 +17,38 @@ export default function CommissionOpinion() {
 
                 <div className="background-title">
 
-                    <Title title="Commission Opinion" nav="Proposals > Commission Opinion" />
+                    <Title title="Parecer da  Comissão" nav="Proposals > Parecer da  Comissão" />
 
 
                 </div>
 
                 <div className="box">
+
+                    <p>Parecer da comissão</p>
+
+                    <div className="display-flex">
+                        <div className="display-grid">
+                            <label htmlFor="yes">Aprovado</label>
+                            <input type="radio" id="yes" name="parecer" />
+                        </div>
+
+                        <div className="display-grid">
+
+                            <label htmlFor="no">Reprovado</label>
+                            <input type="radio" id="no" name="parecer" />
+                        </div>
+                    </div>
+
+
+                    <p>Observações</p>
+
+                    <textarea className="textarea" />
+                </div>
+
+                <div className="display-flex-end">
+                    <Link to="/agenda/view">
+                        <button className="btn-primary">Salvar</button>
+                    </Link>
                 </div>
             </div>
         </div>

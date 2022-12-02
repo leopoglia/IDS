e7bb7e9af88d75c8e1b5218feb11cb3b9b05596e
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-12-01 21:23:36 -0300

    ✔

--- gedesti/src/components/Fixed/Search/Filter/index.tsx
+++ gedesti/src/components/Fixed/Search/Filter/index.tsx
@@ -4,40 +4,72 @@ export default function Filter() {
     return (
         <div className="filter-modal modal">
             <div className="li">
+                <span className="material-symbols-outlined">
+                    home
+                </span>
                 <span>Minhas demandas</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    person
+                </span>
                 <span>Solicitante</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    manage_accounts
+                </span>
                 <span>Gerente</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    workspaces
+                </span>
                 <span>Forúm</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    location_on
+                </span>
                 <span>Departamento</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    crop_free
+                </span>
                 <span>Tamanho</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    link
+                </span>
                 <span>Código PPM</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    draft
+                </span>
                 <span>Código Demanda</span>
             </div>
 
             <div className="li">
+                <span className="material-symbols-outlined">
+                    schedule
+                </span>
                 <span>Status</span>
             </div>
+
+            <div className="hr" />
+
+            <input type="text" />
+            <button className="btn-primary">Filtrar</button>
         </div>
     )
 }   

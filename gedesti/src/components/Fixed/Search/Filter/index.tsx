bb616029698663041b0bb7f6286c837c8e2d9a5e
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-12-01 21:03:22 -0300

    ✔

--- gedesti/src/components/Fixed/Search/Filter/index.tsx
+++ gedesti/src/components/Fixed/Search/Filter/index.tsx
@@ -6,6 +6,38 @@ export default function Filter() {
             <div className="li">
                 <span>Minhas demandas</span>
             </div>
+
+            <div className="li">
+                <span>Solicitante</span>
+            </div>
+
+            <div className="li">
+                <span>Gerente</span>
+            </div>
+
+            <div className="li">
+                <span>Forúm</span>
+            </div>
+
+            <div className="li">
+                <span>Departamento</span>
+            </div>
+
+            <div className="li">
+                <span>Tamanho</span>
+            </div>
+
+            <div className="li">
+                <span>Código PPM</span>
+            </div>
+
+            <div className="li">
+                <span>Código Demanda</span>
+            </div>
+
+            <div className="li">
+                <span>Status</span>
+            </div>
         </div>
     )
 }   

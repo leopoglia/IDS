82c7d0642b4200e5baac4569c6ade3005b6f7860
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2022-12-01 21:31:34 -0300

    ✔

--- gedesti/src/components/Fixed/Search/Filter/index.tsx
+++ gedesti/src/components/Fixed/Search/Filter/index.tsx
@@ -1,75 +1,85 @@
 import "./style.css";
+import { useState } from "react";
 
 export default function Filter() {
+
+    const [filter, setFilter] = useState(false);
+
     return (
         <div className="filter-modal modal">
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     home
                 </span>
                 <span>Minhas demandas</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     person
                 </span>
                 <span>Solicitante</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     manage_accounts
                 </span>
                 <span>Gerente</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     workspaces
                 </span>
                 <span>Forúm</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     location_on
                 </span>
                 <span>Departamento</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     crop_free
                 </span>
                 <span>Tamanho</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     link
                 </span>
                 <span>Código PPM</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     draft
                 </span>
                 <span>Código Demanda</span>
             </div>
 
-            <div className="li">
+            <div className="li" onClick={() => { setFilter(true) }}>
                 <span className="material-symbols-outlined">
                     schedule
                 </span>
                 <span>Status</span>
             </div>
 
-            <div className="hr" />
 
-            <input type="text" />
-            <button className="btn-primary">Filtrar</button>
+            {filter && (
+                <div>
+                    <div className="hr" />
+
+
+                    <input type="text" placeholder="Insira o parametro aqui" />
+                    <button className="btn-primary">Filtrar</button>
+                </div>
+            )}
         </div>
     )
 }   

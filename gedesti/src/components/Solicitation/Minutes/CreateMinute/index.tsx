c3af91bf9d2a462930636e77f356e7a734bf7694
eduardabolgenhagen <bolgenhagene4@gmail.com>
2022-12-19 15:49:16 -0300

    ✔

--- gedesti/src/components/Solicitation/Minutes/CreateMinute/index.tsx
+++ gedesti/src/components/Solicitation/Minutes/CreateMinute/index.tsx
@@ -2,21 +2,24 @@ import Header from "../../../Fixed/Header";
 import Nav from "../../../Fixed/Nav";
 import Title from "../../../Fixed/Search/Title";
 import Editor from "../../Proposals/EditProposalScope/Editor";
+import { useTranslation } from "react-i18next";
 
 export default function CreateMinute() {
+    const { t } = useTranslation();
+
     return (
         <div className="create-minute">
             <Header icon="file_copy" title="createMinute" />
             <Nav />
             <div className="container">
                 <div className="background-title">
-                    <Title title="Criar Ata" nav="Atas > Criar Ata" />
+                    <Title title={t("createMinute")} nav={t("minuteCreateMinute")} />
                 </div>
 
                 <Editor />
 
                 <div className="display-flex-end">
-                    <button className="btn-primary">Salvar</button>
+                    <button className="btn-primary">{t("save")}</button>
                 </div>
 
             </div>

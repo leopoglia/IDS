da20513fddb5d827271e538562e52bf394f2e350
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-01-03 23:30:35 -0300

    ✔️

--- gedesti/src/components/Others/Notifications/index.tsx
+++ gedesti/src/components/Others/Notifications/index.tsx
@@ -4,13 +4,20 @@ import Nav from "../../Fixed/Nav"
 import Title from "../../Fixed/Search/Title";
 import Notification from "./Notification";
 import Footer from "../../Fixed/Footer";
+import { useState } from "react";
 
 export default function Notifications() {
+
+    
+
     return (
         <div className="notifications">
 
-            {/* <div className="barrier"></div>
-            <img className="bia" src="https://i.imgur.com/r004kLD.png"></img> */}
+            <div className="barrier"></div>
+            <img className="bia" src="https://i.imgur.com/r004kLD.png"></img>
+            <div className="bubble modal">
+
+            </div>
 
             <Header icon="notifications" title="notifications" />
             <Nav />

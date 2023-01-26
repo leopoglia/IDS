00ec1b2402846a5a669020d58e691991af9d8bba
K43RU <86034275+Vytor-Rosa@users.noreply.github.com>
2023-01-26 13:41:21 -0300

    ok

--- gedesti/src/services/demandService.ts
+++ gedesti/src/services/demandService.ts
@@ -65,6 +65,44 @@ const Services = {
                 .then(resolve)
                 .catch(reject)
         })
+    },
+    update: function(id: number, demandTitle: String, currentProblem: String, demandObjective: String, costCenter: any, demandStatus: String, score: Number, executionPeriod: Number, requesterRegistration: Number, realBenefit: Number, potentialBenefit: Number, qualitativeBenefit: Number, demandAttachment: any, demandDate: String) {
+        var formData = new FormData();
+
+        let costCenters = [];
+        for (let i = 0; i < costCenter.length; i++) {
+            costCenters.push({ "costCenterCode": costCenter[i] });
+        }
+
+        console.log("cost centers -> ", costCenters)
+
+        let demand = {
+            "demandTitle": demandTitle,
+            "currentProblem": currentProblem,
+            "demandObjective": demandObjective,
+            "costCenter": costCenters,
+            "demandStatus": demandStatus,
+            "score": score,
+            "executionPeriod": executionPeriod,
+            "requesterRegistration": { "workerCode": requesterRegistration },
+            "realBenefit": { "realBenefitCode": realBenefit },
+            "qualitativeBenefit": { "qualitativeBenefitCode": qualitativeBenefit },
+            "potentialBenefit": { "potentialBenefitCode": potentialBenefit },
+            "demandDate": demandDate
+        }
+        formData.append('demand', JSON.stringify(demand));
+        formData.append('demandAttachment', demandAttachment);
+
+        console.log(formData.get("demand"));
+
+        return new Promise((resolve, reject) => {
+            fetch(url + "/" + id, {
+                method: 'PUT',
+                body: formData
+            }).then(function (result) { return result.json(); })
+                .then(resolve)
+                .catch(reject)
+        })
     }
 }
 

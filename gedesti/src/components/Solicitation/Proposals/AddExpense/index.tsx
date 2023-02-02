e51596444e3812ca5eafa7acafc3a499307edd36
ester <ester_girelli@estudante.sesisenai.org.br>
2023-02-02 16:47:57 -0300

    :+1:

--- gedesti/src/components/Solicitation/Proposals/AddExpense/index.tsx
+++ gedesti/src/components/Solicitation/Proposals/AddExpense/index.tsx
@@ -5,8 +5,28 @@ import Nav from "../../../Fixed/Nav";
 import Title from "../../../Fixed/Search/Title";
 import SelectAddExpense from "./SelectAddExpense";
 import "./style.css";
+import { useState } from "react";
+// import { useNavigate } from "react-router-dom";;
+import { toast, ToastContainer, TypeOptions } from 'react-toastify';
+import ButtonAction from "../../Demands/CrateDemand/ButtonAction";
 
 export default function addExpense() {
+
+    // const navigate = useNavigate();
+
+    const nextStep = () => {
+
+        localStorage.getItem("add-expense");
+        let addExpense = JSON.parse(localStorage.getItem("add-expense") || "{}");
+
+        if (addExpense.expenseType === "" || addExpense.expenseProfile === "" || addExpense.periodOfExecutionMonth === "" || addExpense.necessityHoursQuantity === "" ||
+            addExpense.hourValue === "" || addExpense.expenseTotalValue === "") {
+            notify();
+        } else {
+            // navigate('/proposal/execution-costs');
+        }
+    }
+
     return (
         <div className="add-expense">
             <Header title="Adicionar Despesa" icon="add" />
@@ -57,11 +77,29 @@ export default function addExpense() {
                 </div>
 
                 <div className="display-flex-end">
-                    <Link to="/proposal/execution-costs">
+                    {/* <Link to="/proposal/execution-costs">
                         <button className="btn-primary">{t("add")}</button>
-                    </Link>
+                    </Link> */}
+
+                    <div onClick={() => { nextStep() }}>
+                        <ButtonAction click="add"></ButtonAction>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+
+// Notificação de erro ao preencher os campos obrigatórios
+const notify = () => {
+    toast.error('Preencha todos os campos!', {
+        position: "bottom-right",
+        autoClose: 4000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+};

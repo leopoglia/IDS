a059fd166076b41a969bbb694e32dd715619b9b3
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2023-02-23 21:52:32 -0300

    ✔

--- gedesti/src/components/Others/Dashboard/Box/index.tsx
+++ gedesti/src/components/Others/Dashboard/Box/index.tsx
@@ -65,7 +65,6 @@ export default function Box() {
                 <Graphic />
             </div>
 
-
         </div>
     )
 }
--- gedesti/src/components/Others/Dashboard/Graphic/index.tsx
+++ gedesti/src/components/Others/Dashboard/Graphic/index.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
+
+export default function MyChart() {
 
 
-export default function Graphic() {
-    
+    return (
+        <div>
+
+        </div>
+    )
+
 }

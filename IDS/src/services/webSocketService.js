89c2120c76b3ece588527711eca16cc665645a57
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-04-25 15:25:44 -0300

    ✔

---
+++ IDS/src/services/webSocketService.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import SockJS from 'sockjs-client';
+import * as Stomp from "stompjs"
+
+export const WebSocketService = () => {
+    const [stompClient, setStompClient] = useState(null);
+
+    useEffect(() => {
+        const connect = () => {
+            const socket = new SockJS('http://localhost:8443/websocket');
+            const stomp = Stomp.over(socket);
+
+            stomp.connect({}, () => {
+                setStompClient(stomp);
+            }), (error) => {
+                console.log("Connection error: ", error)
+
+                setTimeout(() => {
+                    console.log("Trying to reconnect...")
+                    connect();
+                }, 5000);
+            }
+        }
+
+        connect();
+    }, [])
+
+
+
+    const disconnect = () => {
+        if (stompClient) {
+            stompClient.disconnect();
+        }
+    }
+
+    const send = (destiny, message) => {
+        if (stompClient) {
+            stompClient.send(destiny, {}, JSON.stringify(message));
+        } else {
+            console.log("Connection not established");
+        }
+    }
+
+}

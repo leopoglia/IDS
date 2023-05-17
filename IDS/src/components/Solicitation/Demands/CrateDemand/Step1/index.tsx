264db169182327a428120182f6fe7baa159f30d6
leopoglia <leonardo_poglia@estudante.sc.senai.br>
2023-05-16 21:32:02 -0300

    ✔

--- IDS/src/components/Solicitation/Demands/CrateDemand/Step1/index.tsx
+++ IDS/src/components/Solicitation/Demands/CrateDemand/Step1/index.tsx
@@ -24,10 +24,10 @@ export default function CreateDemands1() {
     useEffect(() => {
         let demand = JSON.parse(localStorage.getItem('demand') || '{}')
 
-        setTitleDemand(demand.titleInput)
-        setCurrentSituation(demand.currentSituation)
-        setObjective(demand.objective)
-        setCostsCenters(demand.costCenter)
+        setTitleDemand(demand?.titleInput)
+        setCurrentSituation(demand?.currentSituation)
+        setObjective(demand?.objective)
+        setCostsCenters(demand?.costCenter)
     }, [])
 
     const [costCenter, setCostCenter] = useState("");

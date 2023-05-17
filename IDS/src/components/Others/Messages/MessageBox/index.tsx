2e5d9b7fc3851ee2309d456ccdf4f94bea7e16a3
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-17 16:34:49 -0300

    ok

--- IDS/src/components/Others/Messages/MessageBox/index.tsx
+++ IDS/src/components/Others/Messages/MessageBox/index.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import "./style.css"
 import ServicesDemand from "../../../../services/demandService";
 import { useTranslation } from "react-i18next";
+import { useContext } from "react";
+import UserContext from "../../../../context/userContext";
+import ServicesMessage from "../../../../services/messageService";
 
 
 export default function Message(props: any) {
@@ -11,7 +14,8 @@ export default function Message(props: any) {
     const [requester, setRequester]: any = useState("");
     const [demandTitle, setDemandTitle]: any = useState("")
     const [imageRequester, setImageRequester]: any = useState("");
-
+    const [sender, setSender]: any = useState("");
+    const worker: any = useContext(UserContext).worker;
 
     useEffect(() => {
 
@@ -21,6 +25,13 @@ export default function Message(props: any) {
             setDemandTitle(response.demandTitle);
         })
 
+        ServicesMessage.findSender(worker.id, props.message?.demandCode).then((response: any) => {
+            setSender(response);
+            console.log(response);
+        })
+
+
+
     }, [props.message]);
 
     return (
@@ -28,10 +39,24 @@ export default function Message(props: any) {
             <div className={"message-" + props.bottom}>
                 <div className="profile">
                     <div className="user-picture">{imageRequester}</div>
-                    <div className="message-name">
-                        <p className="username">{requester} - {demandTitle}</p>
-                        <p className="last-message">{props.message?.sender.workerName}: {props.message?.message}</p>
-                    </div>
+
+                    {
+                        props.message.sender.workerName === worker.name ? (
+                            <div className="message-name">
+                                <p className="username">
+                                    {sender.workerName} - {demandTitle}</p>
+                                <p className="last-message">Você: {props.message?.message}</p>
+                            </div>
+                        ) : (
+                            <div className="message-name">
+                                <p className="username">
+                                    {requester} - {demandTitle}</p>
+                                <p className="last-message">{props.message.sender.workerName}: {props.message?.message}</p>
+                            </div>
+                        )
+                    }
+
+
                 </div>
 
                 <div className="date-horary">

4ad7c924406bd17e8d46a1899bae8ef72010b59e
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-05-23 16:45:12 -0300

    ok

--- IDS/src/components/Others/Notifications/index.tsx
+++ IDS/src/components/Others/Notifications/index.tsx
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.css"
 import Title from "../../Fixed/Search/Title";
 import Notification from "./Notification";
 import Footer from "../../Fixed/Footer";
@@ -8,33 +8,29 @@ import UserContext from "../../../context/userContext";
 import { useTranslation } from "react-i18next";
 import Load from "../../Fixed/Load";
 
+
 export default function Notifications() {
-    const [notifications, setNotifications] = useState([]);
+
+    const [notifications, setNotifications]: any = useState([]);
+    const [haveNotification, setHaveNotification]: any = useState(0);
     const [loading, setLoading] = useState(true);
     const worker = useContext(UserContext).worker;
     const { t } = useTranslation();
 
-    useEffect(() => {
-        fetchNotifications();
-        const timer = setInterval(fetchNotifications, 5);
-        return () => {
-            clearInterval(timer);
-        };
-    }, []);
 
-    const fetchNotifications = () => {
+    useEffect(() => {
         Services.findAll().then((response: any) => {
             setNotifications(response.reverse());
             setLoading(false);
-        });
-    };
+        })
+    }, [notifications])
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+
 
     return (
         <div className="notifications">
+
+
             <div className="container">
                 <div className="backgroud-title">
                     <Title nav="notifications" title="notifications" />
@@ -42,36 +38,52 @@ export default function Notifications() {
 
                 <div className="container-background">
                     <div className="boxNoPadding">
-                        {notifications.length > 0 ? (
-                            notifications.map((notification: any) => {
-                                if (notification.worker.workerCode === worker.id) {
-                                    return (
-                                        <Notification
-                                            key={notification.notificationCode}
-                                            id={notification.notificationCode}
-                                            description={notification.description}
-                                            date={notification.date}
-                                            icon={notification.icon}
-                                            view={notification.visualized}
-                                            type={notification.type}
-                                        />
-                                    );
-                                }
-                                return null;
-                            })
-                        ) : (
-                            <div className="no-results">
-                                <span className="material-symbols-outlined">notifications</span>
-                                <h1>{t("noResults")}</h1>
-                            </div>
-                        )}
+                        {
+                            loading === true ? (
+                                <Load />
+                            ) :
+                                notifications.length > 0 ? (
+                                    notifications.map((notification: any) => {
+                                        if (notification.worker.workerCode === worker.id) {
+
+                                            return (
+                                                <Notification
+                                                    key={notification.notificationCode}
+                                                    id={notification.notificationCode}
+                                                    description={notification.description}
+                                                    date={notification.date}
+                                                    icon={notification.icon}
+                                                    view={notification.visualized}
+                                                    type={notification.type}
+                                                />
+                                            )
+                                        }
+
+                                        if (notification.worker.workerCode === worker.id) {
+                                            setHaveNotification(haveNotification + 1)
+                                        }
+
+
+                                    })
+                                ) : (
+                                    <div className="no-results">
+                                        <span className="material-symbols-outlined">notifications</span>
+                                        <h1>{t("noResults")}</h1>
+                                    </div>
+                                )
+                        }
+
+
                     </div>
                 </div>
 
-                <div className="h45"></div>
+                <div className="h45"></div >
 
                 <Footer />
+
             </div>
+
+
         </div>
     );
 }

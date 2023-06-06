52547b1c714113b520a434f9c29664a0895c56fb
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-06-05 23:02:45 -0300

    👍

--- IDS/src/components/Solicitation/Demands/Workflow/index.tsx
+++ IDS/src/components/Solicitation/Demands/Workflow/index.tsx
@@ -1,28 +1,64 @@
 import { useTranslation } from "react-i18next";
 
 import Title from '../../../Fixed/Search/Title';
+import ServicesDemand from "../../../../services/demandService";
 import './style.css';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 export default function Workerflow() {
 
     const { t } = useTranslation();
 
+    const demandCode: number = parseInt(useParams().id as string);
+    const [stepActual, setStepActual] = useState(0);
+    const steps = ["Criar demanda", "Classificação pelo analista", "Aprovação do gerente", "Complemento do analista", "Criação da proposta", "Inserção em uma pauta de reunião", "Aprovação da comissão", "Aprovação da DG", "Desenvolvimento"];
 
-    const step = (number: any, text: string) => {
+
+    useEffect(() => {
+        ServicesDemand.findById(demandCode).then((demand: any) => {
+            console.log(demand);
+
+            if (demand.demandStatus === "Backlog") {
+                setStepActual(0);
+            } else if (demand.demandStatus === "BacklogRanked") {
+                setStepActual(1);
+            } else if (demand.demandStatus === "BacklogRankedApproved") {
+                setStepActual(2);
+            } else if (demand.demandStatus === "BacklogComplement") {
+                setStepActual(3);
+            } else if (demand.demandStatus === "Assesment") {
+                setStepActual(4);
+            }
+        })
+    }, [])
+
+
+
+    const step = (number: any, text: any, index: number) => {
         return (
-            <div className="step">
-                <div className={"ellipse ellipse-"  + number} >
-                    {number !== "done" ? <span className="number">{number}</span> : <span className="material-symbols-outlined">done</span>
-                    }
+            <div key={index}>
+                <div className="step">
+                    <div className={"ellipse ellipse-" + number} >
+                        {number !== "done" ? <span className="number">{number}</span> : <span className="material-symbols-outlined">done</span>
+                        }
+                    </div>
+
+                    <span className="ellipse-text">
+                        {t(text)}
+                    </span>
                 </div>
 
-                <span className="ellipse-text">
-                    {t(text)}
-                </span>
-            </div>
+                {index !== 8 &&
+                    < div className="background-line">
 
+                        <div className={"line line-" + (index === stepActual - 1 ? true : false)} />
+                    </div>
+                }
+            </div >
         )
     }
+
 
     return (
         <div className="workflow">
@@ -33,15 +69,16 @@ export default function Workerflow() {
                 </div>
 
 
-                <div className="boxNoPadding">
-
-
-                    {step(1, "Criar demanda")}
-                    {step(2, "Criar das")}
-                    {step("done", "Criar das")}
-
-
-
+                <div className="boxNoPadding h100">
+                    {
+                        steps.map((steps, index) => {
+                            if (index <= stepActual) {
+                                return step("done", steps, index);
+                            } else {
+                                return step(index + 1, steps, index);
+                            }
+                        })
+                    }
 
                 </div>
             </div>

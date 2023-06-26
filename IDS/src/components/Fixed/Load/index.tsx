38656993b181ad856203df817d6ece888889b0ee
leopoglia <92129999+leopoglia@users.noreply.github.com>
2023-06-26 19:34:30 -0300

    ✔

--- IDS/src/components/Fixed/Load/index.tsx
+++ IDS/src/components/Fixed/Load/index.tsx
@@ -4,6 +4,13 @@ export default function Spinner() {
     return (
         <div className='load'>
                 <img src="/images/logo.png" alt="" />
+
+            {/* <svg className="loading-spinner" viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+                <circle className="loading-track" cx={50} cy={50} r={5}></circle>
+                <circle className="loading-tail" cx={50} cy={50} r={5}></circle>
+            </svg> */}
+
+
         </div>
     );
 }

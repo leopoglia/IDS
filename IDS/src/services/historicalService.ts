5fe676ea53aa58ed35cc582f903d99a93e242c92
Vytor Rosa <86034275+Vytor-Rosa@users.noreply.github.com>
2023-07-14 16:54:36 -0300

    ok

--- IDS/src/services/historicalService.ts
+++ IDS/src/services/historicalService.ts
@@ -1,49 +1,17 @@
 const url = "http://localhost:8443/api/historical";
 
 const Services = {
-    save: function (demand: Number, historicalAttachment: any, demandRequester: any) {
+   excel: function(demands: any){
         return new Promise((resolve, reject) => {
-
-            fetch(url, {
-                method: 'POST', headers: { 'Content-Type': 'application/json' }, credentials: 'include',
-                body: JSON.stringify({
-                    "demand": { demandCode: demand },
-                    "historicalDate": new Date().getUTCDate() + "/" + (new Date().getUTCMonth() + 1) + "/" + new Date().getUTCFullYear(),
-                    "historicalHour": new Date().getHours() + ":" + new Date().getMinutes(),
-                    "editor": null
-                })
-            }).then(function (result) { return result.json(); })
-                .then(resolve)
-                .catch(resolve)
-        })
-    },
-    findAll: async function () {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'GET', headers: { 'Content-Type': 'application/json' }, credentials: 'include'
-            }).then(function (result) { return result.json(); })
-                .then(resolve)
-                .catch(resolve)
-        })
-    },
-    findById: function (id: number) {
-        return new Promise((resolve, reject) => {
-            fetch(url + "/" + id, {
-                method: 'GET', headers: { 'Content-Type': 'application/json' }, credentials: 'include'
-            }).then(function (result) { return result.json(); })
-                .then(resolve)
-                .catch(resolve)
-        })
-    },
-    delete: function (id: number) {
-        return new Promise((resolve, reject) => {
-            fetch(url + "/" + id, {
-                method: 'DELETE', headers: { 'Content-Type': 'application/json' }, credentials: 'include'
-            }).then(function (result) { return result.json(); })
-                .then(resolve)
-                .catch(resolve)
-        })
-    }
+        fetch(url + "/excel", {
+            method: 'POST',
+            body: JSON.stringify(demands),
+            headers: { 'Content-Type': 'application/json' }, credentials: 'include'
+        }).then(function (result) { return result.json(); })
+            .then(resolve)
+            .catch(resolve)
+    })
+   }
 }
 
 export default Services;
